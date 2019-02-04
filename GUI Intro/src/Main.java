deb0ab9f4bbb1711b0ac2ba0c08d4b2b2cb318eb
mshanker22 <mshanker22@HS-MATH2-06.gvsd.org>
2019-02-04 13:19:14 -0500

    2/4/2019

--- GUI Intro/src/Main.java
+++ GUI Intro/src/Main.java
@@ -1,5 +1,6 @@
 import java.awt.Point;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -8,16 +9,24 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		JFrame myFrame = new JFrame();
-		myFrame.setBounds(100,300,500,300);
+		JFrame myFrame = new JFrame("My First Frame");//JFrame myFrame = new JFrame();
+		myFrame.setBounds(400,400,600,300);
 		myFrame.setLayout(null);
-		JLabel label1 = new JLabel("our first label");
-		//label1.setSize(35,50);
-		label1.setLocation(new Point(35,50));
+		JLabel label1 = new JLabel("my first label");
+		label1.setBounds(40, 30, 100, 30);
 		myFrame.add(label1);
+		JButton button1 = new JButton("my first button");
+		button1.setBounds(180, 30, 100, 30);
+		myFrame.add(button1);
+		//button1.setLocation(new Point(140,30);
+		//button1.setSize(new Dimension(100,30);
+		
+		
 		
 		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		 
 
 		
 	}
